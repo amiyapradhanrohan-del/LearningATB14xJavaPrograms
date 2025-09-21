c6e4894f8f316fde3710f213071d789702b028a2
amiyapradhanrohan-del <amiyapradhanrohan@gmail.com>
2025-09-21 10:12:51 +0530

    Part 2 Java - 13th Sept 2025, all files added part 1

---
+++ src/ex_02_Java_Basics/Lab014_Variables.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basics;
+
+public class Lab014_Variables {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(String args) {
+
+    }
+}
+//we can have to main methods
---
+++ src/ex_02_Java_Basics/Lab020_Interview_QnA2.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics;
+
+public class Lab020_Interview_QnA2 {
+    public static void main(String[] args) {
+       // int enum =10; --> enum is not allowed because it is a reserve keyword
+        int Enum =10;
+     //   System.out.println(enum);
+
+        int amiya$pradhan =90;
+        int Lab020_Interview_QnA2 =123;
+        System.out.println(amiya$pradhan);
+        System.out.println(Lab020_Interview_QnA2);
+
+    }
+}
