c8b5d041352fca5617243231771809ed85d4a6bf
amiyapradhanrohan-del <amiyapradhanrohan@gmail.com>
2025-10-22 10:26:57 +0530

    switch case

---
+++ src/ex_09_Switch/Lab090_Interview.java
@@ -0,0 +1,10 @@
+package ex_09_Switch;
+
+public class Lab090_Interview {
+    public static void main(String[] args) {
+        boolean b = true; //Its an invalid statement because Java switch expression must be of
+        // switch (b) {                // byte,short,int,long,enums and String
+        // }
+
+    }
+}
---
+++ src/ex_09_Switch/Lab091_Interview.java
@@ -0,0 +1,12 @@
+package ex_09_Switch;
+
+public class Lab091_Interview {
+    public static void main(String[] args) {
+        long a11 = 30l;
+        switch ((int) a11){
+
+
+            //Its a valid statement because Java switch expression must be of byte,short,int,long,enums and String
+        }
+    }
+}
