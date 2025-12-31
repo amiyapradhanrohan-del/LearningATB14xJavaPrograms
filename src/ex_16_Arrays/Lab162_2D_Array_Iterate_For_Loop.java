04e7bdc5d7462ec8a867abf749ca9ea1d0c5c6ad
amiyapradhanrohan-del <amiyapradhanrohan@gmail.com>
2025-12-31 22:05:37 +0530

    Arrays in Java

--- src/ex_16_Arrays/Lab162_2D_Array_Iterate_For_Loop.java
+++ src/ex_16_Arrays/Lab162_2D_Array_Iterate_For_Loop.java
@@ -13,7 +13,7 @@ public class Lab162_2D_Array_Iterate_For_Loop {
 
         for (int i = 0; i < matrix.length; i++){
             for (int j = 0; j < matrix[i].length; j++){
-                System.out.print(matrix[i][j]);
+                System.out.print(matrix[i][j]+ " | ");
                 //System.out.println("*);
             }
             System.out.println();
---
+++ src/ex_16_Arrays/Lab163_2D_Right_Triangle.java
@@ -0,0 +1,30 @@
+package ex_16_Arrays;
+
+import java.util.Scanner;
+
+public class Lab163_2D_Right_Triangle {
+    public static void main(String[] args) {
+        //Right hand triangle pattern
+        //How the pattern we want
+        //  //n = 3
+        //  //*
+        //  //**
+        //  //***
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the n for the pattern, e.g n = 3");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+//Output -
+//Enter the n for the pattern, e.g n = 3
+//3
+//*
+//**
+//***
