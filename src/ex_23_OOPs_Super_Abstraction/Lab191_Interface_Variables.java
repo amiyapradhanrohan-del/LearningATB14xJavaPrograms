423db00824d90afd7f10306c7f91310eb7c8fc06
amiyapradhanrohan-del <amiyapradhanrohan@gmail.com>
2026-01-25 23:07:18 +0530

    OOPs Super_Abstraction Concept

--- src/ex_23_OOPs_Super_Abstraction/Lab191_Interface_Variables.java
+++ src/ex_23_OOPs_Super_Abstraction/Lab191_Interface_Variables.java
@@ -16,7 +16,44 @@ class Dutta implements Pramod{
     int aa = 10;
     @Override
     public void display() {
+     //   System.out.println(super.a); //Not possible
         System.out.println(a);
         System.out.println(this.aa);
     }
+}
+class Somya implements TTA{
+
+    @Override
+    public void practice() {
+
+    }
+}
+class Pramod1 implements TTA{
+
+    @Override
+    public void practice() {
+
+    }
+}
+
+interface TTA{
+    void practice();
+
+}
+class Pramod_Mentor implements TTA_Mentor{
+
+    @Override
+    public void teach() {
+
+    }
+}
+class Dipak implements TTA_Mentor{
+
+    @Override
+    public void teach() {
+
+    }
+}
+interface TTA_Mentor{
+    void teach();
 }
