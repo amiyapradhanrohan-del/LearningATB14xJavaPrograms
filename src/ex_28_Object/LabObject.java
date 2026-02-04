38f69a4be93f49c899648c0bd2181d3c795ebdea
amiyapradhanrohan-del <amiyapradhanrohan@gmail.com>
2026-02-04 21:49:09 +0530

    Object in Java

---
+++ src/ex_28_Object/LabObject.java
@@ -0,0 +1,9 @@
+package ex_28_Object;
+
+public class LabObject {
+    public static void main(String[] args) {
+        //we can create an object of object class
+
+        Object o = new Object();
+    }
+}
